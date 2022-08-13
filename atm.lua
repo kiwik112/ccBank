0d2a1e1abe9e74cebfcdd42ceac92de3118e007a
kiwik112 <pakiwi06@gmail.com>
2022-08-13 20:17:47 +0200

    Update atm.lua

--- atm.lua
+++ atm.lua
@@ -5,13 +5,30 @@ function writeln(text)
 end
 
 m = peripheral.wrap("monitor_0")
+
+homeScr:
+
 m.setTextScale(1.5)
 m.setBackgroundColor(colors.blue)
 m.clear()
 m.setCursorPos(2,2)
 m.write("ATM")
+
 event, side, xPos, yPos = os.pullEvent("monitor_touch")
 m.clear()
+m.setTextScale(0.5)
+m.write("Please insert your card.")
+i = 0
+while true do
+    i = i + 1
+    if disk.isPresent("left") do
+        break
+    end
+    if i > 200 do
+        goto homeScr
+    end
+end
+m.clear();
 
 --dis code succs.
 --yes.
